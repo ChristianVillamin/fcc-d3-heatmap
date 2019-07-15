977b6dcce7e7be0652c6545cc2ed828c5cef73bc
Christian Villamin <ChristianVillamin31@gmail.com>
2019-07-15 23:11:19 +0800

    refactored

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-d3.json(
-  'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json'
-).then(dataset => {
-  const svgWidth = 1200;
-  const svgHeight = 800;
-  const svgPadding = 75;
-  const svgPaddingTop = 90;
+const dataURL =
+  'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
 
-  const barHeight = (svgHeight - svgPadding - svgPaddingTop) / 12;
-  const baseTemp = dataset.baseTemperature;
+const svgWidth = 1600;
+const svgHeight = 800;
+const svgPadding = 60;
+const svgPaddingTop = 90;
 
-  const years2 = dataset.monthlyVariance
+d3.select('#container')
+  .style('width', `${svgWidth}px`)
+  .style('height', `${svgHeight}px`);
+
+d3.select('#title').style('left', `${svgWidth / 2}px`);
+d3.select('#description').style('left', `${svgWidth / 2}px`);
+
+// Data
+d3.json(dataURL).then(dataset => visualize(dataset));
+
+const visualize = dataset => {
+  const yearsFiltered = dataset.monthlyVariance
     .map(d => d.year)
     .filter((d, i, s) => s.indexOf(d) === i);
 
-  const barWidth = (svgWidth - svgPadding * 2) / years2.length;
+  const barWidth = (svgWidth - svgPadding * 2) / yearsFiltered.length;
+  const barHeight = (svgHeight - svgPadding - svgPaddingTop) / 12;
+  const baseTemp = dataset.baseTemperature;
 
-  // CONTAINER
-  d3.select('body')
-    .append('div')
-    .attr('id', 'container')
-    .style('position', 'absolute')
-    .style('width', `${svgWidth}px`)
-    .style('height', `${svgHeight}px`)
-    .style('top', '50%')
-    .style('left', '50%')
-    .style('transform', 'translate(-50%, -50%)')
-    .style('box-shadow', '0 0 5px gray');
+  d3.select('#description').text(
+    `1753 - 2015: Base Temp: ${dataset.baseTemperature}°C`
+  );
 
   // SVG
   const svg = d3
@@ -33,36 +35,11 @@ d3.json(
     .append('svg')
     .attr('width', svgWidth)
     .attr('height', svgHeight)
-    .attr('class', 'svg-container')
-    .style('background-color', 'white');
-
-  // TITLE
-  d3.select('#container')
-    .append('h1')
-    .text('Heat Map | Global Land-Surface Temperature')
-    .attr('id', 'title')
-    .style('position', 'absolute')
-    .style('left', `${svgWidth / 2}px`)
-    .style('top', '10px')
-    .style('width', '100%')
-    .style('transform', `translateX(-50%)`)
-    .style('font-size', '25px')
-    .style('text-align', 'center');
-
-  d3.select('#container')
-    .append('h2')
-    .text(`1753 - 2015: Base Temp: ${dataset.baseTemperature}℃`)
-    .attr('id', 'description')
-    .style('position', 'absolute')
-    .style('left', `${svgWidth / 2}px`)
-    .style('top', '50px')
-    .style('width', '100%')
-    .style('transform', `translateX(-50%)`)
-    .style('font-size', '18px')
-    .style('text-align', 'center');
+    .attr('class', 'svg-container');
 
   // X-AXIS
   const years = dataset.monthlyVariance.map(d => new Date(d.year, 0));
+
   const xScale = d3
     .scaleTime()
     .domain([d3.min(years), d3.max(years)])
@@ -91,6 +68,7 @@ d3.json(
     'November',
     'December'
   ];
+
   const yScale = d3
     .scaleBand()
     .domain(months)
@@ -104,49 +82,32 @@ d3.json(
     .attr('id', 'y-axis')
     .call(yAxis);
 
+  // Bars
   const colorRange = [
-    '#fff6e5',
-    '#ffedcc',
+    // '#fff6e5',
+    // '#ffedcc',
     '#ffe4b2',
     '#ffdb99',
     '#ffd27f',
     '#ffc966',
-    '#ffc04c',
+    // '#ffc04c',
     '#ffb732',
-    '#ffae19',
+    // '#ffae19',
     '#ffa500'
   ];
 
-  const hotDomain = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-  // const temperatures = dataset.monthlyVariance
-  //   .map(d => baseTemp + d.variance)
-  //   .sort((a, b) => a - b);
-
   const colorScale = d3
-    .scaleLinear()
-    .domain(hotDomain)
+    .scaleQuantize()
+    // .domain(d3.range(1, 13, 1))
+    .domain([1, 13])
     .range(colorRange);
 
-  const tooltip = d3
-    .select('#container')
-    .append('h2')
-    .text('')
-    .attr('id', 'tooltip')
-    .style('position', 'absolute')
-    // .style('left', `${svgWidth / 2}px`)
-    // .style('top', '50px')
-    .style('transform', `translateX(-50%)`);
+  const tooltip = d3.select('#tooltip');
 
   const selector = d3
-    .select('#container')
-    .append('div')
-    .style('position', 'absolute')
+    .select('#selector')
     .style('width', `${barWidth}px`)
-    .style('height', `${barHeight}px`)
-    .style('background-color', 'black')
-    .style('opacity', '0.5')
-    .style('pointer-events', 'none');
+    .style('height', `${barHeight}px`);
 
   // Legend
   const legend = d3
@@ -166,21 +127,24 @@ d3.json(
     .attr('y', 0)
     .attr('width', `${svgWidth / colorRange.length}px`)
     .attr('height', '100px')
-    // .attr('fill', d => d)
     .attr('class', 'legend')
-    .attr('fill', (d, i) => {
-      console.log(i * 1.3);
-      return colorScale(Math.floor(i * 1.3 + 1));
-    });
+    .attr('fill', (d, i) => colorRange[i]);
 
-  legend
-    .selectAll('rect')
+  const degs = [1, 3, 5, 7, 9, 11];
+
+  d3.select('#container')
+    .selectAll('h4')
     .data(colorRange)
     .enter()
-    .append('h3')
-    .html('HIHII')
-    .attr('x', (d, i) => (svgWidth / colorRange.length) * i)
-    .attr('y', 0);
+    .append('h4')
+    .text((d, i) =>
+      i != degs.length - 1
+        ? `${degs[i]}°C - ${degs[i + 1]}°C `
+        : `${degs[i]}°C +`
+    )
+    .style('left', (d, i) => `${(svgWidth / colorRange.length) * i}px`)
+    .style('top', (d, i) => `${svgHeight + 40}px`)
+    .style('width', `${svgWidth / colorRange.length}px`);
 
   // Bars
   svg
@@ -198,20 +162,15 @@ d3.json(
     .attr('height', `${barHeight}px`)
     .attr('fill', d => colorScale(Math.floor(baseTemp + d.variance)))
     .on('mouseover', (d, i) => {
-      // tooltip
-      // .style('top', `${yScale(months[d.month - 1]) - 125}px`)
-      // .style('left', `${xScale(years[i]) - 100}px`)
-      // .attr('data-year', d.year)
-      // .style('opacity', 0.5);
-
       tooltip
         .html(
-          `${d.year}: ${months[d.month - 1]} <br/> Temp: ${baseTemp +
-            d.variance}℃`
+          `${months[d.month - 1]}: ${d.year}<br/>Temp: ${(
+            baseTemp + d.variance
+          ).toFixed(2)}℃`
         )
         .attr('data-year', d.year)
         .style('left', `${xScale(years[i])}px`)
-        .style('top', `${yScale(months[d.month - 1]) - 50}px`)
+        .style('top', `${yScale(months[d.month - 1]) - 25}px`)
         .style('visibility', 'visible');
 
       selector
@@ -223,6 +182,4 @@ d3.json(
       tooltip.style('visibility', 'hidden');
       selector.style('visibility', 'hidden');
     });
-
-  d3.select('body').on('mouseout', () => console.log('ddd'));
-});
+};
